809838a31274d412e2f5ab098078d8ca5802c0f0
Varun chauhan <115783538+chauhan-varun@users.noreply.github.com>
2025-06-22 13:50:23 +0530

    Create SimpleStorage.sol

---
+++ StorageFactory/SimpleStorage.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract SimpleStorage {
+
+    struct Person {
+        string name;
+        uint256 favNum;
+    }
+
+    Person[] public Persons;
+    mapping (string => uint256) public favNum;
+
+    function set(string memory _name, uint256 _favNum) public {
+        Persons.push(Person(_name, _favNum));
+        favNum[_name]=_favNum;
+
+    }
+
+    function get(string memory _name) public view returns(uint256) {
+        return favNum[_name];
+    }
+
+}
