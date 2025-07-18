8849330595c1d62ae8ca7d166f15f79ddbddc901
Hilmi <muhammedhilmi46@gmail.com>
2025-07-18 20:08:27 +0530

    add NatSpec comments to loop.sol SafeMath.sol payable.sol ens.sol

--- SafeMath.sol
+++ SafeMath.sol
@@ -1,8 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
+/// @title SafeMath Example
+/// @author Varun Chauhan
+/// @notice Demonstrates unchecked addition to avoid overflow errors.
 contract SafeMath {
+
     uint8 public num = 255;
+
+    /// @notice Increments `num` by 1 using unchecked math to bypass overflow checks.
     function add() public {
         unchecked {
             num = num + 1;
